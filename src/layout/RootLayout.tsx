ea644418d36cbef415017568d3745ad000426bd2
Sourave Halder <souravh093@gmail.com>
2024-08-24 20:21:13 +0600

    make layout and route usefull

---
+++ src/layout/RootLayout.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/pages/Shared/Footer";
+import Header from "@/pages/Shared/Header";
+import { Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default RootLayout;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,47 @@
+import AuthLayout from "@/layout/AuthLayout";
+import DashboardLayout from "@/layout/DashboardLayout";
+import RootLayout from "@/layout/RootLayout";
+import Login from "@/pages/Auth/Login";
+import Register from "@/pages/Auth/Register";
+import Error from "@/pages/Error/Error";
+import Home from "@/pages/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    errorElement: <Error />,
+    children: [
+        
+    ]
+  }
+]);
+
+export default router;
